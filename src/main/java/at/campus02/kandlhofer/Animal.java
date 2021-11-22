185c62ac14daa3520aeccb7d2cbe35e6cbce3bce
dkandlhofer <dkandlhofer@know-center.at>
2021-11-22 11:04:39 +0100

    Implemented Animal Class; Implemented Test for Animal

--- src/main/java/at/campus02/kandlhofer/Animal.java
+++ src/main/java/at/campus02/kandlhofer/Animal.java
@@ -1,10 +1,112 @@
 package at.campus02.kandlhofer;
 
-import java.util.*;
+import java.util.Objects;
 
 public class Animal {
 
-    public static void main(String[] args) {
+    private String name;
+    private int age;
+    private String species;
+    private String animalLoud;
 
+    // Konstruktor mit allen 4 Attributen als Input Parameter
+    public Animal(String name, int age, String species, String animalLoud) {
+        this.name = name;
+        setAge(age); // damit das age nur an einer Stelle geprüft werden muss, wird hier der setter aufgerufen
+        this.species = species;
+        this.animalLoud = animalLoud;
+    }
+
+    // getter und setter (ALT + Einfg)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // age kann nicht kleiner 0 sein
+    public void setAge(int age) {
+        this.age = age < 0 ? 0 : age; // wenn age kleiner 0, setze 0 sonst age
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getAnimalLoud() {
+        return animalLoud;
+    }
+
+    public void setAnimalLoud(String animalLoud) {
+        this.animalLoud = animalLoud;
+    }
+
+    // equals und hashcode (ALT + Einfg)
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Animal animal = (Animal) o;
+        return age == animal.age &&
+               Objects.equals(name, animal.name) &&
+               Objects.equals(species, animal.species) &&
+               Objects.equals(animalLoud, animal.animalLoud);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, species, animalLoud);
+    }
+
+    // toString (ALT + Einfg)
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+               "name='" +
+               name +
+               '\'' +
+               ", age=" +
+               age +
+               ", species='" +
+               species +
+               '\'' +
+               ", animalLoud='" +
+               animalLoud +
+               '\'' +
+               '}';
+    }
+
+    // Methods
+    public String giveAnimalLoud() {
+        // animalLoud kann direkt retourniert werden - 2 Möglichkeiten
+        // return this.animalLoud;
+        return getAnimalLoud();
+    }
+
+    public String getNameAndAge() {
+        return "Name: " + name + "; Alter: " + age;
+    }
+
+    public String ageCategory() {
+        if (age > 4) {
+            return "old animal";
+        }
+        if (age > 2) {
+            return "young animal";
+        }
+        return "baby animal";
     }
 }
--- src/test/java/at/campus02/kandlhofer/AnimalTest.java
+++ src/test/java/at/campus02/kandlhofer/AnimalTest.java
@@ -1,20 +1,70 @@
 package at.campus02.kandlhofer;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AnimalTest {
 
-    int a;
+    private Animal dog;
+    private Animal cat;
+    private Animal lion;
 
-    @org.junit.jupiter.api.BeforeEach
+    /**
+     * Vor jedem einzelnen TestCase wird diese Methode aufgerufen
+     * So stehen im die selben Werte (Objekte) für die Tests zur Verfügung
+     */
+    @BeforeEach
     void setUp() {
-        a = 1;
+        dog = new Animal("Bello", 2, "Dog", "Wuff");
+        cat = new Animal("Kitty", 5, "Cat", "Miau");
+        lion = new Animal("Simba", 0, "Lion", "Roarr");
     }
 
     @Test
-    void myFirstTest() {
-        assertTrue(a > 0);
+    @DisplayName("Teste den Konstruktor, ob alle Attribute korrekt gesetzt werden")
+    void constructor() {
+        Animal animal = new Animal("name", 3, "species", "animalLoud");
+        assertEquals("name", animal.getName());
+        assertEquals(3, animal.getAge());
+        assertEquals("species", animal.getSpecies());
+        assertEquals("animalLoud", animal.getAnimalLoud());
+    }
+
+    @Test
+    @DisplayName("Wenn age < 0, dann wird age auf 0 gesetzt")
+    void testAgeHasToBeGreaterThan0() {
+        dog.setAge(-3);
+        assertEquals(0, dog.getAge());
+
+        Animal animal = new Animal("name", -4, "species", "animalLoud");
+        assertEquals(0, animal.getAge());
+    }
+
+    @Test
+    @DisplayName("Teste den Laut des Tieres")
+    void giveAnimalLoud() {
+        assertEquals("Wuff", dog.giveAnimalLoud());
+        assertEquals("Miau", cat.giveAnimalLoud());
+        assertEquals("Roarr", lion.giveAnimalLoud());
+    }
+
+    @Test
+    void getNameAndAge() {
+        assertEquals("Name: Bello; Alter: 2", dog.getNameAndAge());
+        assertEquals("Name: Kitty; Alter: 5", cat.getNameAndAge());
+        assertEquals("Name: Simba; Alter: 0", lion.getNameAndAge());
+    }
+
+    @Test
+    void ageCategory() {
+        assertEquals("baby animal", dog.ageCategory());
+        assertEquals("old animal", cat.ageCategory());
+        assertEquals("baby animal", lion.ageCategory());
+
+        dog.setAge(3);
+        assertEquals("young animal", dog.ageCategory());
     }
 }
